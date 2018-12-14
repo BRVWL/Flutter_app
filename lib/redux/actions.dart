bbe77964a548e32049ace4d7f7114e4310a48827
Artem Semenchenko <jindnbua@gmail.com>
2018-12-14 16:58:50 +0200

    Added add item / remove item / remove all items actions

---
+++ lib/redux/actions.dart
@@ -0,0 +1,18 @@
+import 'package:my_app/model/model.dart';
+
+class AddItemAction {
+  static int _id = 0;
+  final String item;
+  AddItemAction(this.item) {
+    _id++;
+  }
+  int get id => _id;
+}
+
+class RemoveItemAction {
+  final Item item;
+  RemoveItemAction(this.item);
+}
+
+class RemoveAllItemsAction {}
+
