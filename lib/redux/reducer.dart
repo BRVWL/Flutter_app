20cd48bc3faca3dc67e3ca256444f689831e026e
Artem Semenchenko <jindnbua@gmail.com>
2018-12-14 17:30:17 +0200

    Added reducer for add and del

--- lib/redux/reducer.dart
+++ lib/redux/reducer.dart
@@ -2,7 +2,27 @@ import 'package:my_app/model/model.dart';
 import 'package:my_app/redux/actions.dart';
 
 AppState appStateReducer(AppState state, action) {
-  return AppState();
+  return AppState(
+    items: itemReducer(state.items, action),
+  );
 }
 
+List<Item> itemReducer(List<Item> state, action) {
+  
+  if(action is AddItemAction) {
+    return 
+      []
+      ..addAll(state)
+      ..add(Item(id: action.id, body: action.item));
+  }
 
+  if(action is RemoveItemAction) {
+    return List.unmodifiable(List.from(state)..remove(action.item));
+  }
+
+  if(action is RemoveAllItemsAction) {
+    return [];
+  }
+
+  return state;
+}
