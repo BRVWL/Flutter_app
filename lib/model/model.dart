0709e0ce3d34590e73ea7b7c0d07ec5ec3294132
Artem Semenchenko <jindnbua@gmail.com>
2018-12-14 16:46:48 +0200

    Created model ITEM and initial State with immutable aaray of items

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -4,12 +4,30 @@ class Item {
   final int id;
   final String body;
 
+  // constructor
   Item({
     @required this.id,
     @required this.body
   });
+
+  // For imuttable
+  Item copyWith({ int id, String body }) {
+    return Item(
+      id: id ?? this.id,
+      body: body ?? this.body,
+    );
+  }
+
 }
 
 class AppState {
-  
+  final List<Item> items;
+  // constructor
+  AppState({
+    @required this.items,
+  });
+
+  // INITIAL STATE
+  AppState.initialState() : items = List.unmodifiable(<Item>[]);
+
 }
