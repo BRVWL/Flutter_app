b4161c7b5a7e1c0d4b02b81c5dc3bb83aa5d6a7b
Artem Semenchenko <jindnbua@gmail.com>
2018-12-14 04:04:00 +0200

    map list

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@
 
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 
 // You can use a relative import, i.e. `import 'category.dart;'` or
 // a package import, as shown below.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
 import 'category.dart';
+import 'category_route.dart';
 
 const _categoryName = 'Cake';
 const _categoryIcon = Icons.cake;
@@ -29,16 +29,7 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+      home: CategoryRoute(),
     );
   }
 }
