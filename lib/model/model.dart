2d32f5bbfd01b3db1a9a2085ce590f2befb2b3f4
Artem Semenchenko <jindnbua@gmail.com>
2018-12-14 16:33:32 +0200

    Item

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/foundation.dart';
 
 class Item {
-  
+  final int id;
+  final String body;
+
+  Item({
+    @required this.id,
+    @required this.body
+  });
 }
 
 class AppState {
